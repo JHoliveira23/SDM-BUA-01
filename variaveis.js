7afef1a4880c93c6623e31fa5dd696c55928ded1
JHoliveira23 <joaohenriquep69@gmail.com>
2025-03-11 10:12:30 -0300

    feat: Trabalhando com variaveis

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+var idade = 39;
+let nome = "João";
+let ativo = true;
+const PI = 3.14
+
+// Conversão de String para número
+let num = Number("42")
+// Conversão de número para String
+let str = String(100)
+let outraStr = (42).toString()
+
+let float = parseFloat("5.7")
+let inteiro = parseInt("10.5")
+console.log(num, float, inteiro)
+console.log(idade)
+if(true){
+    let cidade = "Osasco" 
+    console.log(cidade)   
+}
+console.log(PI)
+
