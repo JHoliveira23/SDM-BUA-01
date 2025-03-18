3798bd975310f32ce3733ca80b2a025bc317d8fb
JHoliveira23 <joaohenriquep69@gmail.com>
2025-03-18 11:04:24 -0300

    Trabalhando com funções

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+function ola(nome){
+    return `Olá, ${nome}`
+}
+
+console.log(ola("Milho"))
+
+
+const soma = function(a, b){
+    return a + b
+}
+
+console.log(soma(11, 22)) 
+
+const mult = (x, y) => x * y;
+console.log(mult(11, 3))
